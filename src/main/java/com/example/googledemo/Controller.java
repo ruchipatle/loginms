9df2806ab3dd78a7bd73a9ac1c7b69b29b2763ee
Ruchi Patle <ruchi.patle.ext@lafargeholcim.com>
2021-05-17 15:45:39 +0530

    get user details

--- src/main/java/com/example/googledemo/Controller.java
+++ src/main/java/com/example/googledemo/Controller.java
@@ -1,13 +1,36 @@
 package com.example.googledemo;
 
+import java.util.Collection;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lh.cif.workflow.model.UserMaster;
+import com.lh.cif.workflow.security.CurrentUser;
+import com.lh.cif.workflow.security.UserPrincipal;
 
 @RestController
 public class Controller {
 
     @GetMapping("/hello")
     public String hello() {
+    	
         return "hello";
     }
+    
+    @GetMapping("/user/me")
+	public String getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+		boolean isAdmin = false;
+		Collection<? extends GrantedAuthority> userAuthorities = userPrincipal.getAuthorities();
+		for(GrantedAuthority role : userAuthorities) {
+			if(role.getAuthority().equals("Admin")) {
+				isAdmin = true;
+				break;
+			}
+		}
+		return "User details are: "+userPrincipal.getEmail()+" "+userPrincipal.getName()+" "+userPrincipal.getUsername()+" "+userPrincipal.getId()+" "+userPrincipal.getAuthorities();
+	}
+ 
 }
